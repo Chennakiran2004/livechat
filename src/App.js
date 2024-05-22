9e1b3cfde3f718dc382378be7f87b939e9ec8bab
Chennakiran2004 <kirankumarchenna2004@gmail.com>
2024-05-22 11:04:18 +0530

    live chat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "./styles.css";
-import Chat from "./components/Chat";
-import Login from "./components/Login";
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -9,8 +7,6 @@ const App = () => {
   return (
     <div className="App">
       <h1>Live Chat Application</h1>
-      {/* {user ? <Chat user={user} /> : <Login setUser={setUser} />} */}
-      <Chat />
     </div>
   );
 };
